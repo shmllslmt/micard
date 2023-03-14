47aff5c6f8df6cdace099f4c767c50c8ff6f48fa
shmll <shmllslmt@gmail.com>
2023-03-14 15:47:41 +0800

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,69 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black54,
-        body: Container(),
+        backgroundColor: Colors.purple[50],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/unicorn.jpg'),
+                radius: 70.0,
+              ),
+              Text(
+                'Shamilla S.',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'LECTURER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text('data engineering, aspect-based sentiment analysis'),
+              SizedBox(
+                height: 20,
+                width: double.infinity,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 50.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.deepPurple,
+                  ),
+                  title: Text(
+                    '+6017-12345678',
+                    style: TextStyle(color: Colors.deepPurpleAccent),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 50.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.deepPurple,
+                  ),
+                  title: Text(
+                    'nurshamilla@sc.edu.my',
+                    style: TextStyle(color: Colors.deepPurpleAccent),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
-
